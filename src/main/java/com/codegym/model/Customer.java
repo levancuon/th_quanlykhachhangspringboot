29f0892329c562291e8e5d432cc5e075e1a6f649
levancuon <levancuongprovip@gmail.com>
2024-07-01 10:36:58 +0700

    awndkaw

---
+++ src/main/java/com/codegym/model/Customer.java
@@ -0,0 +1,17 @@
+package com.codegym.model;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customer")
+@Data
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
